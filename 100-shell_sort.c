28e5d0bccd2dd6e3c51a04d8b44bc2c0f2a6dce1
MichaelDecent <michaeldecent2@gmail.com>
2023-06-25 00:34:30 +0000

    update on sorting_algorithms project

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -21,51 +21,24 @@ void swap_ints(int *val_1, int *val_2)
  *
  * @array: the array to be sorted
  * @size: size of the array
-
+*/
 void shell_sort(int *array, size_t size)
 {
-	int i, j, gap = 1;
+	size_t i, j, gap;
 	int temp;
 
         if (array == NULL || size < 2)
                 return;
 
-	 Genrate the Knuth sequence
-	while (gap < (int)size)
-		gap *= 3 + 1;
-        iterating the array and progressively reducing the gap
-	for (gap = gap / 3; gap > 0; gap /= 3)
-	{
-		for (i = gap; i < (int)size; i++)
-		{
-			comparing elements far apart based on the gap between them
-			temp = array[i];
-
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-				array[j] = array[j - gap];
-			array[j] = temp;
-		}
-		print_array(array, size);
-	}
-}**/
-void shell_sort(int *array, size_t size)
-{
-	size_t gap, i, j;
-	int temp;
-
-	if (array == NULL || size < 2)
-		return;
-
-	/* Generate the gap base of the knuth sequence */
-	for (gap = 1; gap < size; gap = gap * 3 + 1)
+	/*Genrate the Knuth sequence*/
+	for(gap = 1; gap < size; gap *= 3 + 1)
 		;
-
-	/* Reduce the gap by dividing by 3 per iteration */
+        /*iterating the array and progressively reducing the gap*/
 	for (gap = gap / 3; gap > 0; gap /= 3)
 	{
-		/* Ensure the gap is within the array index */
 		for (i = gap; i < size; i++)
 		{
+			/*comparing elements far apart based on the gap between them*/
 			temp = array[i];
 
 			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
