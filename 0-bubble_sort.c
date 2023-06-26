8600d21291a78062d20cd0599a5877d0a43b63bc
alyalsayed <alyalsayed@kali.aly.com>
2023-06-26 17:47:55 +0200

    updating bubble sort  and fixing errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,28 @@
 #include "sort.h"
 
 /**
- * swap - swaps two values in an array
- * @val_1: first value
- * @val_2: second value
+ * bubble_sort - sort list with bubble
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
-void swap(int *a, int *b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-/**
-  * bubble_sort - A function for bubble sorting.
-  * @array: The array to sort.
-  * @size: THe length of the aaray.
-  */
 void bubble_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+	size_t n = 0, t = size;
+	int holder;
 
-	int swapped = 1;
-    for (size_t i = 0; i < size - 1 && swapped; i++) {
-        swapped = 0;
-        for (size_t j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j+1]) {
-                swap(&array[j], &array[j+1]);
-                swapped = 1;
+	if (array == NULL)
+		return;
+	for (t = size; t > 0; t--)
+	{
+		for (n = 0; n < size - 1; n++)
+		{
+			if (array[n] > array[n + 1])
+			{
+				holder = array[n];
+				array[n] = array[n + 1];
+				array[n + 1] = holder;
 				print_array(array, size);
 			}
 		}
 	}
 }
-
