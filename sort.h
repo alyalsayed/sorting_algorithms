85e6e88268e5accd295fec0a9fb0dfc5bb682e05
alyalsayed <alyalsayed@kali.aly.com>
2023-06-26 18:35:53 +0200

    updating   and fixing errors

--- sort.h
+++ sort.h
@@ -21,15 +21,21 @@ typedef struct listint_s
 } listint_t;
 
 
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
 
