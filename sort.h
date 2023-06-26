1f9f45de23191eeac0c974d72d48e524f0b75776
alyalsayed <alyalsayed@kali.aly.com>
2023-06-26 17:50:52 +0200

    updating bubble sort  and fixing errors

--- sort.h
+++ sort.h
@@ -1,8 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -17,7 +16,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
-void swap(int *a, int *b);
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
