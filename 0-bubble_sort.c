48ffbd51c9bee926fcbe5324ad5ab35edd35e7a5
alyalsayed <alyalsayed@kali.aly.com>
2023-06-26 17:58:15 +0200

    updating bubble sort  and fixing errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,19 +1,17 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * using the Bubble sort algorithm
- * @array: The array to be sorted
+ * bubble_sort - sort list with bubble
+ * @array: The array to be printed
  * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t n, t;
+	size_t n = 0, t = size;
 	int holder;
 
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
-
 	for (t = size; t > 0; t--)
 	{
 		for (n = 0; n < size - 1; n++)
