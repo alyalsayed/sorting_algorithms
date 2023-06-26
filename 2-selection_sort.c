7461926358d0c531213392357376d75e97eecf88
alyalsayed <alyalsayed@kali.aly.com>
2023-06-26 18:05:07 +0200

    updating selection sort  and fixing errors

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,40 +1,31 @@
 #include "sort.h"
 
 /**
- * swap_ints - swaps two values in an array
- * @val_1: first value
- * @val_2: second value
- */
-void swap_ints(int *val_1, int *val_2)
-{
-	int tmp;
-
-	tmp = *val_1;
-	*val_1 = *val_2;
-	*val_2 = tmp;
-}
-
-/**
- * selection_sort - A function that use selection sort algorithm.
- * @array: An array to sort.
- * @size: The size of the array.
+ * selection_sort - sort list with selection_sort
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+	size_t i, c, less, tmp, swap;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
 	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
+		for (less = i, c = i; c < size; c++)
+			if (array[c] < array[less])
+			{
+				less = c;
+				swap = 1;
+			}
+		if (swap == 1)
 		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
-		}
-		if (min_idx != i)
-		{
-			swap_ints(&array[min_idx], &array[i]);
+			tmp = array[less];
+			array[less] = array[i];
+			array[i] = tmp;
 			print_array(array, size);
+			swap = 0;
 		}
 	}
 }
