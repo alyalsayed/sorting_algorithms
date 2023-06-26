c1af7712b1ee92ef451cbe11fde6b9885a22faf8
alyalsayed <alyalsayed@kali.aly.com>
2023-06-26 20:07:36 +0200

    breaking : uploading bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,20 +8,22 @@
   */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
+	size_t i = 0, j = 0;
+	int aux = 0;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	if (array == NULL || size == 0)
+		return;
+	for (; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = aux;
+				print_array(array, size);
+			}
+		}
+	}
 }
