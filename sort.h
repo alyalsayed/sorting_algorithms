bd6c253745b72e81725bd4e236190ca10399ae99
alyalsayed <alyalsayed56@yahoo.com>
2023-06-26 19:56:41 +0300

    upload bubble selection quick sort

--- sort.h
+++ sort.h
@@ -1,10 +1,6 @@
-#ifndef SORT_H
-#define SORT_H
-
-#include <stdio.h>
+#ifndef __SORT__
+#define __SORT__
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -15,27 +11,39 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
+/*======================================================================*/
+/*=================         Printing Algoritms        ==================*/
+/*======================================================================*/
 
-void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+/*======================================================================*/
+/*=================          sorting Algoritms        ==================*/
+/*======================================================================*/
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void quick_s(int *array, int lo, int hi, size_t size);
+void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
+void merge_partition(size_t lo, size_t hi, int *array, int *base);
+void swap_root(int *array, size_t root, size_t hi, size_t size);
 
-#endif
-
+#endif /* __SORT__ */
